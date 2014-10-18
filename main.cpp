694939cd16da267080bbcf8b1062ec277545e641
Amila Perera <perera.amila@gmail.com>
2014-10-18 06:46:09 +0530

    Center main window on the screen

--- main.cpp
+++ main.cpp
@@ -1,19 +1,40 @@
 #include <QApplication>
 #include "Ui/MainWindow.h"
 
+#include <QDesktopWidget>
+
 enum ExitStatus {
     SUCCESS,
     PRE_CHECK_FAILED,
 };
 
+static void centerMainWindow(MainWindow *w);
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    centerMainWindow(&w);
+
     w.show();
     if (!w.PreCheck()) {
         return PRE_CHECK_FAILED;
     }
 
     return a.exec();
+}
+
+void centerMainWindow(MainWindow *w)
+{
+    QDesktopWidget *desktop = QApplication::desktop();
+    int screenWidth = desktop->width();
+    int screenHeight = desktop->height();
+
+    int width = w->frameGeometry().width();
+    int height = w->frameGeometry().height();
+    w->setGeometry((screenWidth / 2) - (width / 2),
+                   (screenHeight / 2) - (height / 2),
+                   width,
+                   height);
 }
